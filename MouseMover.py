e309e0104f6897600e6fe8684a2843d890a45fce
AlekUp24 <alekp24@gmail.com>
2024-02-02 14:54:41 +0100

    Log file added

--- MouseMover.py
+++ MouseMover.py
@@ -2,6 +2,15 @@ import pyautogui as p
 import datetime as d
 import random, time
 
+
+def write_log(curr, new):
+    curr , new = str(curr) , str(new)
+    file_path = "C:\\Users\\hubi4\\Desktop\\Hubert.txt" 
+    log_file = open(file_path,'a')
+    log_file.write(F"Position changed from {curr} to {new} \n")
+    log_file.close()
+
+
 curr_pos = p.position()
 width, height = p.size()
 time_counter = 0
@@ -21,6 +30,10 @@ while True:
         y = random.randint(100,height-100)
         z = random.uniform(0.3,1.5) # randomize mouse speed
         p.moveTo(x,y,z)
+        try:
+            write_log(curr_pos , p.position())
+        except:
+            print("couldn't log to the file")
         curr_pos = p.position()
 
     end = d.datetime.now() 
